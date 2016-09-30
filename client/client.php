179a402c4163205a092030f3a22f79061c9da973
JellyChen_Office <451453325@qq.com>
2016-09-30 16:11:32 +0800

    Echo服务器

--- client/client.php
+++ client/client.php
@@ -6,22 +6,59 @@ class Client
 
     public function __construct()
     {
-        $this->client = new swoole_client(SWOOLE_SOCK_TCP);
+        $this->client = new swoole_client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_ASYNC);
+
+        $this->client->on('Connect', array($this, 'onConnect'));
+        $this->client->on('Receive', array($this, 'onReceive'));
+        $this->client->on('Close', array($this, 'onClose'));
+        $this->client->on('Error', array($this, 'onError'));
     }
 
     public function connect()
     {
-        if (!$this->client->connect("127.0.0.1", 9501, 1)) {
-            echo "Error: {$this->client->errMsg}[{$this->client->errCode}]\n";
+        $fp = $this->client->connect('127.0.0.1', 9501, 1);
+        if (!$fp) {
+            echo "Error: {$fp->errMsg}[{$fp->errCode}]\n";
+
+            return;
         }
-
-        fwrite(STDOUT, "请输入消息：");
-        $msg = trim(fgets(STDIN));
-        $this->client->send($msg);
-
-        $message = $this->client->recv();
-        echo "Get Message From Server:{$message}\n";
     }
+
+    public function onReceive($cli, $data)
+    {
+        echo "Get Message From Server:{$data}\n";
+    }
+
+    public function onConnect($cli)
+    {
+        fwrite(STDOUT, "Enter Message:");
+        swoole_event_add(STDIN, function ($fp) {
+            global $cli;
+            fwrite(STDOUT, "Enter Msg:");
+            $msg = trim(fgets(STDIN));
+            $cli->send($msg);
+        });
+    }
+
+    public function onClose($cli)
+    {
+        echo "Client close connection\n";
+    }
+
+    public function onError($cli)
+    {
+
+    }
+
+    public function send($msg)
+    {
+        $this->client->send($msg);
+    }
+
+    public function isConnected(){
+        return $this->client->isConnected();
+    }
+
 }
 
 $client = new Client();
--- server/server.php
+++ server/server.php
@@ -9,6 +9,7 @@ class Server
         $this->serv = new swoole_server("0.0.0.0", 9501);
         $this->serv->set(array(
             'worker_num'      => 8,
+            'task_worker_num' => 8,
             'daemonize'       => false,
         ));
 
@@ -16,6 +17,9 @@ class Server
         $this->serv->on('Connect', array($this, 'onConnect'));
         $this->serv->on('Receive', array($this, 'onReceive'));
         $this->serv->on('Close', array($this, 'onClose'));
+
+        $this->serv->on('Task', array($this, 'onTask'));
+        $this->serv->on('Finish', array($this, 'onFinish'));
 
         $this->serv->start();
     }
@@ -27,19 +31,47 @@ class Server
 
     public function onConnect($serv, $fd, $from_id)
     {
-        $serv->send($fd, "Hello {$fd}!");
+        echo "Client {$fd} connect\n";
     }
 
     public function onReceive(swoole_server $serv, $fd, $from_id, $data)
     {
         echo "Get Message From Client {$fd}:{$data}\n";
-        $serv->send($fd, $data);
+        // send a task to task worker.
+        $param = array(
+            'fd' => $fd,
+        );
+        // start a task
+        $serv->task(json_encode($param));
+
+        echo "Continue Handle Worker\n";
     }
 
     public function onClose($serv, $fd, $from_id)
     {
         echo "Client {$fd} close connection\n";
     }
+
+    public function onTask($serv, $task_id, $from_id, $data)
+    {
+        echo "This Task {$task_id} from Worker {$from_id}\n";
+        echo "Data:{$data}\n";
+        for ($i = 0; $i < 10; $i++) {
+            sleep(1);
+            echo "Task {$task_id} Handle {$i} times...";
+        }
+        $fd = json_encode($data, true)['fd'];
+        $serv->send($fd, "Data in Task {$task_id}");
+
+        return "Task {$task_id}'s result";
+    }
+
+    public function onFinish($serv, $task_id, $data)
+    {
+        echo "Task {$task_id} finish\n";
+        echo "Result:{$data}\n";
+    }
+
 }
 
 // 启动服务器
