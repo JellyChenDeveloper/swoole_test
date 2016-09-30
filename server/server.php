2c9f294f2cecdd75019ee9b0a3853383c9af6621
JellyChen_Office <451453325@qq.com>
2016-09-30 11:36:39 +0800

    Echo服务器

--- server/server.php
+++ server/server.php
@@ -1,13 +1,15 @@
 <?php
+
 class Server
 {
     private $serv;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->serv = new swoole_server("0.0.0.0", 9501);
         $this->serv->set(array(
-            'worker_num' => 8,
-            'daemonize' => false,
+            'worker_num'      => 8,
+            'daemonize'       => false,
         ));
 
         $this->serv->on('Start', array($this, 'onStart'));
@@ -18,22 +20,27 @@ class Server
         $this->serv->start();
     }
 
-    public function onStart( $serv ) {
+    public function onStart($serv)
+    {
         echo "Start\n";
     }
 
-    public function onConnect( $serv, $fd, $from_id ) {
-        $serv->send( $fd, "Hello {$fd}!" );
+    public function onConnect($serv, $fd, $from_id)
+    {
+        $serv->send($fd, "Hello {$fd}!");
     }
 
-    public function onReceive( swoole_server $serv, $fd, $from_id, $data ) {
+    public function onReceive(swoole_server $serv, $fd, $from_id, $data)
+    {
         echo "Get Message From Client {$fd}:{$data}\n";
         $serv->send($fd, $data);
     }
 
-    public function onClose( $serv, $fd, $from_id ) {
+    public function onClose($serv, $fd, $from_id)
+    {
         echo "Client {$fd} close connection\n";
     }
 }
+
 // 启动服务器
 $server = new Server();
