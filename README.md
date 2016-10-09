897a8ccf44ac21e9ef274a6550918f7f2ea592ab
JellyChen_Office <451453325@qq.com>
2016-10-09 14:08:23 +0800

    official sample code sort

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ### swoole测试项目
 
-swoole学习，教程参见[Easy Swoole](https://www.gitbook.com/book/linkeddestiny/easy-swoole/details)
+swoole学习，教程参见[swoole文档及入门教程](https://github.com/LinkedDestiny/swoole-doc)
 
 ## 官方源码
 [{$root}/official_code](official_code)
 ## 学习代码
-[{$root}/mycode](mycode)
+[{$root}/my code](mycode)
--- official_code/03/swoole_heartbeat_server.php
+++ official_code/03/swoole_heartbeat_server.php
@@ -2,17 +2,18 @@
 
 class TimerServer
 {
-	private $serv;
+    private $serv;
 
-	public function __construct() {
-		$this->serv = new swoole_server("0.0.0.0", 9501);
+    public function __construct()
+    {
+        $this->serv = new swoole_server("0.0.0.0", 9501);
         $this->serv->set(array(
-            'worker_num' => 8,
-            'daemonize' => false,
-            'max_request' => 10000,
-            'dispatch_mode' => 2,
-            'debug_mode'=> 1 ,
-            'heartbeat_check_interval' => 10
+            'worker_num'               => 8,
+            'daemonize'                => false,
+            'max_request'              => 10000,
+            'dispatch_mode'            => 2,
+            'debug_mode'               => 1,
+            'heartbeat_check_interval' => 10,
         ));
 
         $this->serv->on('WorkerStart', array($this, 'onWorkerStart'));
@@ -20,26 +21,36 @@ class TimerServer
         $this->serv->on('Receive', array($this, 'onReceive'));
         $this->serv->on('Close', array($this, 'onClose'));
         $this->serv->start();
-	}
+    }
 
-	public function onWorkerStart( $serv , $worker_id) {
-		// 在Worker进程开启时绑定定时器
+    public function onWorkerStart($serv, $worker_id)
+    {
+        // 在Worker进程开启时绑定定时器
         echo "onWorkerStart\n";
         // 只有当worker_id为0时才添加定时器,避免重复添加
-        if( $worker_id == 0 ) {
-        	$serv->addtimer(5000);
+        if ($worker_id == 0) {
+            if (!$serv->taskworker) {
+                $serv->tick(5000, function ($worker_id) {
+                    echo $worker_id . "\n";
+                });
+            } else {
+                $serv->addtimer(5000);
+            }
         }
     }
 
-    public function onConnect( $serv, $fd, $from_id ) {
+    public function onConnect($serv, $fd, $from_id)
+    {
         echo "Client {$fd} connect\n";
     }
 
-    public function onReceive( swoole_server $serv, $fd, $from_id, $data ) {
+    public function onReceive(swoole_server $serv, $fd, $from_id, $data)
+    {
         echo "Get Message From Client {$fd}:{$data}\n";
     }
 
-    public function onClose( $serv, $fd, $from_id ) {
+    public function onClose($serv, $fd, $from_id)
+    {
         echo "Client {$fd} close connection\n";
     }
 }
